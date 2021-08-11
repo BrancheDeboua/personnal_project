baf865c5aaf1bc8029402b7b04cde88ced35b355
= <Laforme.s@hotmail.com>
2021-08-11 00:47:53 -0400

    First Dev commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # personnal_project
+
+Hello!
+
+This is my personnal project to learn more about programming and node.js!
+It will stay up on my raspberry pi so I can connect to it wherever I want.
+I will do whatever I want with this project, so far I've implemented theses things:
+
+-
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use((req, res) => {
     res.send("My personnal project");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is up');
 });
